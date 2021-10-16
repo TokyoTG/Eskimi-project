d558603b9a20d24a36e997ef8ba40339be90ea1c
tokyotg <tboynonilekan@gmail.com>
2021-10-16 23:02:02 +0100

    basic crud

---
+++ app/Http/Controllers/CampaignController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Campaign;
+
+class CampaignController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $campaigns = Campaign::all()->toArray();
+        return array_reverse($campaigns);    
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $campaign = new Campaign([
+            'name' => $request->input('name'),
+            'from' => $request->input('from'),
+            'to' => $request->input('to'),
+            'daily_budget' => $request->input('daily_budget'),
+            'total_budget' => $request->input('total_budget')
+        ]);
+        $campaign->save();
+
+        return response()->json('Campaign created!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $campaign = Campaign::find($id);
+        $images = $campaign->images;
+        return response()->json($campaign);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $campaign = Campaign::find($id);
+        $campaign->update($request->all());
+
+        return response()->json('Campaign updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $campaign = Campaign::find($id);
+        $campaign->delete();
+
+        return response()->json('Campaign deleted!');
+    }
+}
---
+++ resources/js/components/EditCampaign.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <h3 class="text-center">Edit Campaign</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <form @submit.prevent="updateCampaign">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" v-model="campaign.name">
+                    </div>
+                     <div class="form-group">
+                        <label>Start Date</label>
+                        <input type="datetime" class="form-control" v-model="campaign.from" required>
+                    </div>
+                    <div class="form-group">
+                        <label>End Date</label>
+                        <input type="datetime" class="form-control" v-model="campaign.to" required>
+                    </div>
+                     <div class="form-group">
+                        <label>Daily Budget</label>
+                        <input type="number" class="form-control" v-model="campaign.daily_budget" required>
+                    </div>
+                     <div class="form-group">
+                        <label>Total Budget</label>
+                        <input type="number" class="form-control" v-model="campaign.total_budget" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {
+                campaign: {},
+                images:[],
+            }
+        },
+        created() {
+            this.axios
+                .get(`http://localhost:8000/api/campaigns/${this.$route.params.id}`)
+                .then((res) => {
+                    this.campaign = res.data;
+                    this.images = res.data.images;
+                });
+        },
+        methods: {
+            updateCampaign() {
+                this.axios
+                    .patch(`http://localhost:8000/api/campaigns/${this.$route.params.id}`, this.campaign)
+                    .then((res) => {
+                        this.$router.push({ name: 'home' });
+                    });
+            }
+        }
+    }
+</script>
